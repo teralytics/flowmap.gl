87823ade2912ffa565470ae55e7692511f33aba1
Ilya Boyandin <ilyabo@gmail.com>
2017-12-28 12:34:38 +0100

    Added two separate stories: fp32 and fp64

--- stories/index.tsx
+++ stories/index.tsx
@@ -2,4 +2,6 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import Example from './Example';
 
-storiesOf('FlowMapLayer', module).add('simple', () => <Example />);
+storiesOf('FlowMapLayer', module)
+  .add('fp32', () => <Example fp64={false} />)
+  .add('fp64', () => <Example fp64={true} />);
