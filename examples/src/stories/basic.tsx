ae72addbb1f55ceb925e287a213dbfa06ae59c8e
Ilya Boyandin <ilyabo@gmail.com>
2020-01-28 13:00:27 +0100

    Added a dark mode example

--- examples/src/stories/basic.tsx
+++ examples/src/stories/basic.tsx
@@ -80,6 +80,55 @@ storiesOf('Basic', module)
     }),
   )
   .add(
+    'dark mode',
+    pipe(
+      withStats,
+      withFetchJson('locations', './data/locations.json'),
+      withFetchJson('flows', './data/flows-2016.json'),
+    )(({ locations, flows }: any) => {
+      const colors = {
+        darkMode: true,
+        flows: {
+          scheme: [
+            'rgb(0, 22, 61)',
+            'rgb(0, 27, 62)',
+            'rgb(0, 36, 68)',
+            'rgb(0, 48, 77)',
+            'rgb(3, 65, 91)',
+            'rgb(48, 87, 109)',
+            'rgb(85, 115, 133)',
+            'rgb(129, 149, 162)',
+            'rgb(179, 191, 197)',
+            'rgb(240, 240, 240)',
+          ],
+        },
+        locationAreas: {
+          normal: '#334',
+        },
+        outlineColor: '#000',
+      };
+
+      return (
+        <>
+          <FlowMap
+            colors={colors}
+            mapStyle="mapbox://styles/mapbox/dark-v10"
+            mixBlendMode="screen"
+            getLocationId={getLocationId}
+            flows={flows}
+            locations={locations}
+            showLocationAreas={false}
+            initialViewState={getViewStateForFeatures(locations, [window.innerWidth, window.innerHeight])}
+            mapboxAccessToken={mapboxAccessToken}
+          />
+          <LegendBox bottom={35} left={10} style={{ backgroundColor: '#000', color: '#fff' }}>
+            <LocationTotalsLegend colors={colors} />
+          </LegendBox>
+        </>
+      );
+    }),
+  )
+  .add(
     'custom dark mode color scheme',
     pipe(
       withStats,
