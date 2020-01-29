32a3b91fb6a0b38a4afc595c509fe78eb21e3b2d
Ilya Boyandin <ilyabo@gmail.com>
2020-01-29 23:58:08 +0100

    Ordered stories

--- examples/src/stories/index.tsx
+++ examples/src/stories/index.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Teralytics
+ * Copyright 2020 Teralytics
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,9 @@ import { mapboxAccessToken } from '../index';
 import pipe from '../utils/pipe';
 import { withFetchJson } from '../utils/withFetch';
 import withStats from '../utils/withStats';
+import ClusteringExample from '../components/ClusteringExample';
+import withSheetsFetch from '../utils/withSheetsFetch';
+import Example from '../components/Example';
 
 const getLocationId = (loc: Location) => loc.properties.abbr;
 
@@ -118,7 +121,10 @@ storiesOf('Basic', module)
             flows={flows}
             locations={locations}
             showLocationAreas={false}
-            initialViewState={getViewStateForFeatures(locations, [window.innerWidth, window.innerHeight])}
+            initialViewState={{
+              ...getViewStateForFeatures(locations, [window.innerWidth, window.innerHeight]),
+              altitude: 1.5,
+            }}
             mapboxAccessToken={mapboxAccessToken}
           />
           <LegendBox bottom={35} left={10} style={{ backgroundColor: '#000', color: '#fff' }}>
@@ -524,4 +530,111 @@ storiesOf('Basic', module)
         </>
       );
     }),
+  );
+
+storiesOf('Cluster on zoom', module)
+  .add(
+    'basic',
+    pipe(
+      withStats,
+      withFetchJson('locations', './data/locations.json'),
+      withFetchJson('flows', './data/flows-2016.json'),
+    )(({ locations, flows }: any) => (
+      <ClusteringExample
+        locations={locations}
+        flows={flows}
+        getLocationId={(loc: Location) => loc.properties.abbr}
+        getLocationCentroid={(loc: Location) => loc.properties.centroid}
+        getFlowOriginId={(flow: Flow) => flow.origin}
+        getFlowDestId={(flow: Flow) => flow.dest}
+        getFlowMagnitude={(flow: Flow) => +flow.count}
+      />
+    )),
+  )
+  .add(
+    'NL commuters',
+    pipe(
+      withStats,
+      withSheetsFetch('1Oe3zM219uSfJ3sjdRT90SAK2kU3xIvzdcCW6cwTsAuc'),
+    )(({ locations, flows }: any) => (
+      <ClusteringExample
+        locations={locations}
+        flows={flows}
+        getLocationId={(loc: Location) => loc.id}
+        getLocationCentroid={(loc: Location): [number, number] => [+loc.lon, +loc.lat]}
+        getFlowOriginId={(flow: Flow) => flow.origin}
+        getFlowDestId={(flow: Flow) => flow.dest}
+        getFlowMagnitude={(flow: Flow) => +flow.count}
+      />
+    )),
+  );
+
+storiesOf('Other datasets', module)
+  .add(
+    'London bicycle hire',
+    pipe(
+      withStats,
+      withSheetsFetch('1Z6dVVFFrdooHIs8xnJ_O7eM5bhS5KscCi7G_k0jUNDI'),
+    )(({ locations, flows }: any) => (
+      <Example
+        flows={flows}
+        locations={locations}
+        getLocationId={(loc: Location) => loc.id}
+        getLocationCentroid={(location: Location): [number, number] => [+location.lon, +location.lat]}
+        getFlowOriginId={(flow: Flow) => flow.origin}
+        getFlowDestId={(flow: Flow) => flow.dest}
+        getFlowMagnitude={(flow: Flow) => +flow.count}
+      />
+    )),
+  )
+  .add(
+    'NYC citibike',
+    pipe(
+      withStats,
+      withSheetsFetch('1Aum0anWxPx6bHyfcFXWCCTE8u0xtfenIls_kPAJEDIA'),
+    )(({ locations, flows }: any) => (
+      <Example
+        flows={flows}
+        locations={locations}
+        getLocationId={(loc: Location) => loc.id}
+        getLocationCentroid={(location: Location): [number, number] => [+location.lon, +location.lat]}
+        getFlowOriginId={(flow: Flow) => flow.origin}
+        getFlowDestId={(flow: Flow) => flow.dest}
+        getFlowMagnitude={(flow: Flow) => +flow.count}
+      />
+    )),
+  )
+  .add(
+    'Chicago taxis',
+    pipe(
+      withStats,
+      withSheetsFetch('1fhX98NFv5gAkkjB2YFCm50-fplFpmWVAZby3dmm9cgQ'),
+    )(({ locations, flows }: any) => (
+      <Example
+        flows={flows}
+        locations={locations}
+        getLocationId={(loc: Location) => loc.id}
+        getLocationCentroid={(location: Location): [number, number] => [+location.lon, +location.lat]}
+        getFlowOriginId={(flow: Flow) => flow.origin}
+        getFlowDestId={(flow: Flow) => flow.dest}
+        getFlowMagnitude={(flow: Flow) => +flow.count}
+      />
+    )),
+  )
+  .add(
+    'NL commuters',
+    pipe(
+      withStats,
+      withSheetsFetch('1Oe3zM219uSfJ3sjdRT90SAK2kU3xIvzdcCW6cwTsAuc'),
+    )(({ locations, flows }: any) => (
+      <Example
+        flows={flows}
+        locations={locations}
+        getLocationId={(loc: Location) => loc.id}
+        getLocationCentroid={(location: Location): [number, number] => [+location.lon, +location.lat]}
+        getFlowOriginId={(flow: Flow) => flow.origin}
+        getFlowDestId={(flow: Flow) => flow.dest}
+        getFlowMagnitude={(flow: Flow) => +flow.count}
+      />
+    )),
   );
